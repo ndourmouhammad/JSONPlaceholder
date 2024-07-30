8577af2fc03afafa4ef1bf1d520cdf8e34537679
ndourmouhammad <ndourmouhammad@gmail.com>
2024-07-30 19:24:14 +0000

    Ajout du projet

---
+++ src/app/article-form/article-form.component.ts
@@ -0,0 +1,89 @@
+import Swal from 'sweetalert2';
+import { Component, EventEmitter, Input, OnChanges, Output, SimpleChanges } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ArticleService } from '../article.service';
+import { ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  standalone: true,
+  imports: [
+    ReactiveFormsModule,
+  ],
+  selector: 'app-article-form',
+  templateUrl: './article-form.component.html',
+  styleUrls: ['./article-form.component.css']
+})
+export class ArticleFormComponent implements OnChanges {
+
+  articleForm: FormGroup;
+  @Input() article: any; // Reçoit l'article à modifier
+  @Output() articleSubmitted = new EventEmitter<any>();
+
+  constructor(private fb: FormBuilder, private articleService: ArticleService) {
+    this.articleForm = this.fb.group({
+      title: ['', Validators.required],
+      body: ['', Validators.required] // Assurez-vous que ce nom est correct
+    });
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['article'] && this.article) {
+      this.articleForm.patchValue(this.article);
+    }
+  }
+
+  
+
+  onSubmit(): void {
+    const articleData = this.articleForm.value;
+
+    Swal.fire({
+      title: 'Êtes-vous sûr ?',
+      text: 'Voulez-vous soumettre ces informations ?',
+      icon: 'question',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Oui, soumettre !',
+      cancelButtonText: 'Annuler'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        if (this.article && this.article.id) {
+          // Mise à jour de l'article existant
+          this.articleService.updateArticle(this.article.id, articleData).subscribe(updatedArticle => {
+            Swal.fire(
+              'Mis à jour !',
+              'L\'article a été mis à jour avec succès.',
+              'success'
+            );
+            this.articleSubmitted.emit(updatedArticle);
+          }, error => {
+            Swal.fire(
+              'Erreur !',
+              'Une erreur est survenue lors de la mise à jour de l\'article.',
+              'error'
+            );
+          });
+        } else {
+          // Création d'un nouvel article
+          this.articleService.createArticle(articleData).subscribe(newArticle => {
+            Swal.fire(
+              'Créé !',
+              'L\'article a été créé avec succès.',
+              'success'
+            );
+            this.articleSubmitted.emit(newArticle);
+          }, error => {
+            Swal.fire(
+              'Erreur !',
+              'Une erreur est survenue lors de la création de l\'article.',
+              'error'
+            );
+          });
+        }
+      }
+    });
+  }
+}
+
+
---
+++ src/app/article-list/article-list.component.ts
@@ -0,0 +1,74 @@
+import { Component, OnInit } from '@angular/core';
+import { ArticleService } from '../article.service';
+import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { ArticleFormComponent } from '../article-form/article-form.component';
+import Swal from 'sweetalert2';
+
+@Component({
+  standalone: true,
+  imports: [RouterLink, CommonModule, ArticleFormComponent],
+  selector: 'app-article-list',
+  templateUrl: './article-list.component.html',
+  styleUrls: ['./article-list.component.css']
+})
+export class ArticleListComponent implements OnInit {
+
+  articles: any[] = [];
+  isFormVisible = false;
+  selectedArticle: any;
+
+  constructor(private articleService: ArticleService) {}
+
+  ngOnInit(): void {
+    this.loadArticles();
+  }
+
+  loadArticles(): void {
+    this.articleService.getArticles().subscribe(data => {
+      this.articles = data;
+    });
+  }
+
+  addNewArticle(): void {
+    this.selectedArticle = null;
+    this.isFormVisible = true;
+  }
+
+  editArticle(article: any): void {
+    this.selectedArticle = article;
+    this.isFormVisible = true;
+  }
+
+  clearSelectedArticle(): void {
+    this.selectedArticle = null;
+    this.isFormVisible = false;
+  }
+
+  deleteArticle(id: number): void {
+    Swal.fire({
+      title: 'Êtes-vous sûr ?',
+      text: 'Vous ne pourrez pas revenir en arrière après cette action !',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Oui, supprimer !',
+      cancelButtonText: 'Annuler'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.articleService.deleteArticle(id).subscribe(() => {
+          this.articles = this.articles.filter(article => article.id !== id);
+          Swal.fire('Supprimé !', 'L\'article a été supprimé.', 'success');
+        }, error => {
+          Swal.fire('Erreur !', 'Une erreur est survenue lors de la suppression de l\'article.', 'error');
+        });
+      }
+    });
+  }
+
+  onArticleAddedOrUpdated(article: any): void {
+    this.loadArticles();
+    this.isFormVisible = false;
+  }
+}
